6aa6f522f05723bdf3f38702db0ce60ec1ebf795
imkarthi16 <karthiveera@gmail.com>
2022-09-18 20:12:14 +0530

    working with addteam

---
+++ addTeam.js
@@ -0,0 +1,42 @@
+
+
+
+let localTeam = JSON.parse(localStorage.getItem("teamArray"));
+let localPlayers = JSON.parse(localStorage.getItem("playerArray"));
+
+$("#addteamform").submit(function (e) {
+
+
+    e.preventDefault();
+
+
+    let inp_val = $("#inp1").val();
+    let shortName = '';
+    for(let i=0;i<inp_val.length;i++){
+        if(i == 0){
+            shortName += inp_val[i++].toUpperCase();
+            continue;
+        }else if(inp_val[i] == ' '){
+            shortName += inp_val[++i].toUpperCase();
+            i++;
+        }
+    }
+
+    let addData = {
+        "id": localTeam.length,
+        "teamFullName":$("#inp1").val() ,
+        "sName": shortName,
+        
+        "teamIcon": $("#inp3").val(),
+        "WonCount": $("#inp4").val(),
+
+
+    }
+
+   localTeam.push(addData);
+   localStorage.setItem("teamArray", JSON.stringify(localTeam)); 
+
+   location.href = `teams.html?name=${addData.sName}`;
+})
+
+      
